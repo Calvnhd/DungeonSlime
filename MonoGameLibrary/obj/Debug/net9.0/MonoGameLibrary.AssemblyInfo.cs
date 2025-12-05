11c58927a5540807844554477f042463637e6683
Calvin Davidson <82812675+Calvnhd@users.noreply.github.com>
2025-12-06 00:57:13 +1100

    add content

--- MonoGameLibrary/obj/Debug/net9.0/MonoGameLibrary.AssemblyInfo.cs
+++ MonoGameLibrary/obj/Debug/net9.0/MonoGameLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be622e0a63503581730206705ea8e9d38a4b37f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d5baa423ad683fdf26b7da4db342b36dbeb270")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
