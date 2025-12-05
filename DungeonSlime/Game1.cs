59a8bac4a1ea2d3a9c6d18795adfd70436478096
Calvin Davidson <82812675+Calvnhd@users.noreply.github.com>
2025-12-06 01:12:57 +1100

    tut 6 - learning to draw

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -44,8 +44,24 @@ public class Game1 : Core
         // Begin the sprite batch to prepare for rendering.
         SpriteBatch.Begin();
 
-        // Draw the logo texture
-        SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+    // Draw the texture.
+    SpriteBatch.Draw(
+        _logo,                              // texture
+        new Vector2(                        // position
+            Window.ClientBounds.Width,
+            Window.ClientBounds.Height) * 0.5f,
+        null,                               // sourceRectangle
+        Color.White,                        // color
+        0.0f,                               // rotation
+        new Vector2(                        // origin
+            _logo.Width,
+            _logo.Height) * 0.5f,
+        1.0f,                               // scale
+        SpriteEffects.FlipHorizontally |    // effects
+        SpriteEffects.FlipVertically,
+        0.0f                                // layerDepth
+    );
+
 
         // Always end the sprite batch when finished.
         SpriteBatch.End();
